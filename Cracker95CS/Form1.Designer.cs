ed3b73dc4925d926d70356e06c6f57910328785f
s1522711 <s1522711@gmail.com>
2023-09-11 20:23:27 +0300

    Add project files.

---
+++ Cracker95CS/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Cracker95CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            OutputKeyBox = new TextBox();
+            GenerateBtn = new Button();
+            KeyTypeSelectBox = new GroupBox();
+            OemKeySel = new RadioButton();
+            CdKeySel = new RadioButton();
+            CopyBtn = new Button();
+            ValidatorInputBox = new TextBox();
+            ValidatorBtn = new Button();
+            ValidatorPasteBtn = new Button();
+            ValidatorOutputLabel = new Label();
+            pictureBox1 = new PictureBox();
+            KeyTypeSelectBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // OutputKeyBox
+            // 
+            OutputKeyBox.Location = new Point(12, 12);
+            OutputKeyBox.Name = "OutputKeyBox";
+            OutputKeyBox.ReadOnly = true;
+            OutputKeyBox.Size = new Size(156, 23);
+            OutputKeyBox.TabIndex = 0;
+            // 
+            // GenerateBtn
+            // 
+            GenerateBtn.Location = new Point(174, 12);
+            GenerateBtn.Name = "GenerateBtn";
+            GenerateBtn.Size = new Size(75, 23);
+            GenerateBtn.TabIndex = 1;
+            GenerateBtn.Text = "Generate!";
+            GenerateBtn.UseVisualStyleBackColor = true;
+            GenerateBtn.Click += GenerateBtn_Click;
+            // 
+            // KeyTypeSelectBox
+            // 
+            KeyTypeSelectBox.Controls.Add(OemKeySel);
+            KeyTypeSelectBox.Controls.Add(CdKeySel);
+            KeyTypeSelectBox.Location = new Point(216, 41);
+            KeyTypeSelectBox.Name = "KeyTypeSelectBox";
+            KeyTypeSelectBox.Size = new Size(85, 78);
+            KeyTypeSelectBox.TabIndex = 2;
+            KeyTypeSelectBox.TabStop = false;
+            KeyTypeSelectBox.Text = "Key Type";
+            // 
+            // OemKeySel
+            // 
+            OemKeySel.AutoSize = true;
+            OemKeySel.Location = new Point(6, 47);
+            OemKeySel.Name = "OemKeySel";
+            OemKeySel.Size = new Size(73, 19);
+            OemKeySel.TabIndex = 4;
+            OemKeySel.Text = "OEM Key";
+            OemKeySel.UseVisualStyleBackColor = true;
+            // 
+            // CdKeySel
+            // 
+            CdKeySel.AutoSize = true;
+            CdKeySel.Checked = true;
+            CdKeySel.Location = new Point(6, 22);
+            CdKeySel.Name = "CdKeySel";
+            CdKeySel.Size = new Size(63, 19);
+            CdKeySel.TabIndex = 3;
+            CdKeySel.TabStop = true;
+            CdKeySel.Text = "CD Key";
+            CdKeySel.UseVisualStyleBackColor = true;
+            // 
+            // CopyBtn
+            // 
+            CopyBtn.Location = new Point(255, 12);
+            CopyBtn.Name = "CopyBtn";
+            CopyBtn.Size = new Size(75, 23);
+            CopyBtn.TabIndex = 4;
+            CopyBtn.Text = "Copy!";
+            CopyBtn.UseVisualStyleBackColor = true;
+            CopyBtn.Click += CopyBtn_Click;
+            // 
+            // ValidatorInputBox
+            // 
+            ValidatorInputBox.Location = new Point(12, 231);
+            ValidatorInputBox.Name = "ValidatorInputBox";
+            ValidatorInputBox.PlaceholderText = "Enter key to validate";
+            ValidatorInputBox.Size = new Size(156, 23);
+            ValidatorInputBox.TabIndex = 5;
+            // 
+            // ValidatorBtn
+            // 
+            ValidatorBtn.Location = new Point(174, 231);
+            ValidatorBtn.Name = "ValidatorBtn";
+            ValidatorBtn.Size = new Size(75, 23);
+            ValidatorBtn.TabIndex = 6;
+            ValidatorBtn.Text = "Validate!";
+            ValidatorBtn.UseVisualStyleBackColor = true;
+            ValidatorBtn.Click += ValidatorBtn_Click;
+            // 
+            // ValidatorPasteBtn
+            // 
+            ValidatorPasteBtn.Location = new Point(255, 231);
+            ValidatorPasteBtn.Name = "ValidatorPasteBtn";
+            ValidatorPasteBtn.Size = new Size(75, 23);
+            ValidatorPasteBtn.TabIndex = 7;
+            ValidatorPasteBtn.Text = "Paste!";
+            ValidatorPasteBtn.UseVisualStyleBackColor = true;
+            ValidatorPasteBtn.Click += ValidatorPasteBtn_Click;
+            // 
+            // ValidatorOutputLabel
+            // 
+            ValidatorOutputLabel.AutoSize = true;
+            ValidatorOutputLabel.Location = new Point(12, 262);
+            ValidatorOutputLabel.Name = "ValidatorOutputLabel";
+            ValidatorOutputLabel.Size = new Size(38, 15);
+            ValidatorOutputLabel.TabIndex = 8;
+            ValidatorOutputLabel.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.trolla;
+            pictureBox1.Location = new Point(49, 79);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(94, 96);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(356, 286);
+            Controls.Add(pictureBox1);
+            Controls.Add(ValidatorOutputLabel);
+            Controls.Add(ValidatorPasteBtn);
+            Controls.Add(ValidatorBtn);
+            Controls.Add(ValidatorInputBox);
+            Controls.Add(CopyBtn);
+            Controls.Add(KeyTypeSelectBox);
+            Controls.Add(GenerateBtn);
+            Controls.Add(OutputKeyBox);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimumSize = new Size(372, 325);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cracker95 C#";
+            Load += Form1_Load;
+            KeyTypeSelectBox.ResumeLayout(false);
+            KeyTypeSelectBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox OutputKeyBox;
+        private Button GenerateBtn;
+        private GroupBox KeyTypeSelectBox;
+        private RadioButton OemKeySel;
+        private RadioButton CdKeySel;
+        private Button CopyBtn;
+        private TextBox ValidatorInputBox;
+        private Button ValidatorBtn;
+        private Button ValidatorPasteBtn;
+        private Label ValidatorOutputLabel;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Cracker95CS/Form1.cs
@@ -0,0 +1,171 @@
+namespace Cracker95CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateBtn_Click(object sender, EventArgs e) // keygen code
+        {
+            // initialising needed shit
+            OutputKeyBox.Text = string.Empty;
+            Random r = new Random();
+
+            if (CdKeySel.Checked == true) // check what key type is selected
+            {
+                // initialise needed vars
+                string CdCDBox1, CdCDBox2;
+                CdCDBox1 = string.Empty;
+                CdCDBox2 = "1";
+
+                CdCDBox1 = r.Next(100, 365).ToString(); // generate box 1
+
+                while (CdCDBox2.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum() % 7 != 0) // generate box 2
+                {
+                    // ValidatorInputBox.Text = ValidatorInputBox.Text + CdCDBox2 + "\n";
+                    CdCDBox2 = r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(1, 7).ToString();
+                }
+
+                OutputKeyBox.Text = CdCDBox1 + "-" + CdCDBox2; // output generated CD key
+            }
+            else if (OemKeySel.Checked == true)
+            {
+                // initialise needed vars
+                string OemCDBox1P1, OemCDBox1P2, OemCDBox2, OemBox3;
+                OemCDBox1P1 = string.Empty;
+                OemCDBox1P2 = string.Empty;
+                OemCDBox2 = "1";
+                OemBox3 = string.Empty;
+
+                // generate both parts of box 1
+                OemCDBox1P1 = r.Next(100, 365).ToString();
+                OemCDBox1P2 = r.Next(95, 99).ToString();
+
+                while (OemCDBox2.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum() % 7 != 0) // generate box 2
+                {
+                    // ValidatorInputBox.Text = ValidatorInputBox.Text + CdCDBox2 + "\n";
+                    OemCDBox2 = "0" + r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(0, 9).ToString() + r.Next(1, 7).ToString();
+                }
+
+                OemBox3 = r.Next(10000, 99999).ToString(); // generate last 5 digits of key (box 5)
+
+                OutputKeyBox.Text = OemCDBox1P1 + OemCDBox1P2 + "-OEM-" + OemCDBox2 + "-" + OemBox3; // output generated OEM key
+            }
+        }
+
+        private void CopyBtn_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(OutputKeyBox.Text);
+        }
+
+        private void ValidatorPasteBtn_Click(object sender, EventArgs e)
+        {
+            ValidatorInputBox.Text = Clipboard.GetText();
+        }
+
+        private void ValidatorBtn_Click(object sender, EventArgs e) // validator start
+        {
+            try
+            {
+                if (ValidatorInputBox.Text.ToLower().Contains("oem")) // check if key is oem key
+                {
+                    // 01234567890123456789012
+                    // AAABB-OEM-CCCCCCC-DDDDD
+                    // initialising needed vars
+                    string OemCDBox1P1, OemCDBox1P2, OemCDBox2, OemBox3, OemErrors;
+                    OemCDBox1P1 = ValidatorInputBox.Text[0].ToString() + ValidatorInputBox.Text[1].ToString() + ValidatorInputBox.Text[2].ToString();
+                    OemCDBox1P2 = ValidatorInputBox.Text[3].ToString() + ValidatorInputBox.Text[4].ToString();
+                    OemCDBox2 = ValidatorInputBox.Text[10].ToString() + ValidatorInputBox.Text[11].ToString() + ValidatorInputBox.Text[12].ToString() + ValidatorInputBox.Text[13].ToString() + ValidatorInputBox.Text[14].ToString() + ValidatorInputBox.Text[15].ToString() + ValidatorInputBox.Text[16].ToString();
+                    OemBox3 = ValidatorInputBox.Text[18].ToString() + ValidatorInputBox.Text[19].ToString() + ValidatorInputBox.Text[20].ToString() + ValidatorInputBox.Text[21].ToString() + ValidatorInputBox.Text[22].ToString();
+
+                    Int32 ValidityScore = 0;
+                    OemErrors = "";
+
+                    if (001 <= int.Parse(OemCDBox1P1) && int.Parse(OemCDBox1P1) <= 366) { ValidityScore += 1; } // check if first 3 chars of box 1 are allowed
+                    else { OemErrors += "First 3 digits of the first box are not between 001 and 366, "; }
+
+                    if (OemCDBox1P2 == "95" || OemCDBox1P2 == "96" || OemCDBox1P2 == "97" || OemCDBox1P2 == "98" || OemCDBox1P2 == "99" || OemCDBox1P2 == "00" || OemCDBox1P2 == "01" || OemCDBox1P2 == "02" || OemCDBox1P2 == "03") { ValidityScore += 1; } // check if the last 2 chars in box 1 are allowed
+                    else { OemErrors += "Last 2 digits of the first box are not 95,96 ,97,98,99,00,01,02 or 03, "; }
+
+                    if (ValidatorInputBox.Text[5].ToString() == "-" && ValidatorInputBox.Text[9].ToString() == "-" && ValidatorInputBox.Text[17].ToString() == "-") { ValidityScore += 1; } // check if the dashes are dashes
+                    else { OemErrors += "Characters 6, 10 and 18 are not dashes, "; }
+
+                    if (OemCDBox2.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum() % 7 == 0) { ValidityScore += 1; } // check sum of digits in box 2
+                    else { OemErrors += "the sum of the characters in the 2nd box isnt divisible by 7, "; }
+
+                    if (OemCDBox2[0].ToString() == "0") { ValidityScore += 1; } // check if first char in box 2 is 0
+                    else { OemErrors += "The first digit of the 2nd box must be 0, "; }
+
+                    if (ValidatorInputBox.TextLength == 23) { ValidityScore += 1; } // check if key is 23 chars long
+                    else { OemErrors += "The key must be 23 characters long"; }
+
+
+                    if (ValidityScore == 6) // check if enough validity checks passed
+                    {
+                        ValidatorOutputLabel.ForeColor = Color.Green;
+                        ValidatorOutputLabel.Text = "OEM Key is valid!";
+                    }
+                    else
+                    {
+                        ValidatorOutputLabel.ForeColor = Color.Red;
+                        ValidatorOutputLabel.Text = "OEM Key is not valid: " + OemErrors;
+                    }
+                }
+                else // cd key checks
+                {
+                    // initialising most of the needed vars
+                    string CDBox1 = ValidatorInputBox.Text[0].ToString() + ValidatorInputBox.Text[1].ToString() + ValidatorInputBox.Text[2].ToString();
+                    string CDBox2 = ValidatorInputBox.Text[4].ToString() + ValidatorInputBox.Text[5].ToString() + ValidatorInputBox.Text[6].ToString() + ValidatorInputBox.Text[7].ToString() + ValidatorInputBox.Text[8].ToString() + ValidatorInputBox.Text[9].ToString() + ValidatorInputBox.Text[10].ToString();
+                    ValidatorOutputLabel.Text = CDBox1 + CDBox2;
+                    int ValidityScore = 0;
+                    string KeyErrors = "";
+
+                    if (CDBox1 != "333" && CDBox1 != "444" && CDBox1 != "555" && CDBox1 != "666" && CDBox1 != "777" && CDBox1 != "888" && CDBox1 != "999") // check if the first box isnt equal to blacklisted values
+                    {
+                        ValidityScore += 1;
+                    }
+                    else { KeyErrors += "The Digits In The First Box Cant Be 333, 444, 555, 666, 777, 888 or 999, "; }
+
+                    if (CDBox2.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum() % 7 == 0) // check sum of digits in second box
+                    {
+                        ValidityScore += 1;
+                    }
+                    else { KeyErrors += "Sum of Digits In Key Isnt Devisable By 7, "; }
+
+                    if (CDBox2[7] != 0 && CDBox2[7] < 8) // check if last digit is higher than 7 or is 0
+                    {
+                        ValidityScore += 1;
+                    }
+                    else { KeyErrors += "The last digit of the key must be lower than 8 and cant be 0, "; }
+
+                    if (ValidatorInputBox.Text.Length == 11) // check key length
+                    {
+                        ValidityScore += 1;
+                    }
+                    else { KeyErrors += "Key lenght isnt 11"; }
+
+
+                    if (ValidityScore == 4) { ValidatorOutputLabel.ForeColor = Color.Green; ValidatorOutputLabel.Text = "CD Key Valid!"; } // check if enough validity checks have been passed
+                    else
+                    {
+                        ValidatorOutputLabel.ForeColor = Color.Red;
+                        ValidatorOutputLabel.Text = "CD Key Invalid: " + KeyErrors;
+                    }
+                }
+            }
+            catch (Exception IndexOutOfRangeException)
+            {
+                Console.WriteLine(IndexOutOfRangeException);
+                ValidatorOutputLabel.ForeColor = Color.Red;
+                ValidatorOutputLabel.Text = "Key length must be 11 for CD Keys and 23 for OEM Keys";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ValidatorOutputLabel.Text = ""; // make validator output label invisible on load
+        }
+    }
+}
